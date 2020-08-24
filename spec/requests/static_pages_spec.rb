29eed09501d0ebcc9a382cf9e86138e81a0554db
Jou Hattori <j.h178bz@gmail.com>
2020-08-24 22:35:01 +0900

    各viewのテストを追加

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe "StaticPages", type: :request do
+  describe "home_page" do
+    it "responds succesfully" do
+      get root_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "about_page" do
+    it "responds succesfully" do
+      get about_path
+      expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "terms_page" do
+    it "responds succesfully" do
+      get terms_path
+      expect(response).to have_http_status(200)
+    end
+  end
+end
