2bfa7063c435de19608fe68e4914d02617e32a1d
Jou Hattori <j.h178bz@gmail.com>
2020-08-26 20:20:05 +0900

    devise日本語化

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,7 @@ module MyApp
         helper_specs: false,
         routing_specs: false
     end
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
