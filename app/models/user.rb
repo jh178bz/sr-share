1b6920ef3b658fbd2db63107d780192d777a8dea
Jou Hattori <j.h178bz@gmail.com>
2020-08-28 18:17:33 +0900

    user_nameを登録可能に実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   def self.guest
     find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = 'password'
+      user.name = 'テストユーザー'
     end
   end
 end
