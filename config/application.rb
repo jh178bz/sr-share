8de3bc0af86d522c471a3fa1282ab4b551ca6a3b
Jou Hattori <j.h178bz@gmail.com>
2020-08-30 08:53:22 +0900

    follow機能のjs読み込み

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,6 @@ module MyApp
     end
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
