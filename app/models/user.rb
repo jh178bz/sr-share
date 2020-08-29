b1433e1b92808f338dc5e55cb68517c2f235d147
Jou Hattori <j.h178bz@gmail.com>
2020-08-29 21:52:59 +0900

    プロフ画像をフォロー一覧に追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   validates :name, presence: true, length: { maximum: 10 }
+  mount_uploader :image, ImageUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
