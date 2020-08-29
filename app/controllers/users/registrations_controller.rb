af75a787f6c140a70cb569d6e0420a7662dcba01
Jou Hattori <j.h178bz@gmail.com>
2020-08-29 21:50:39 +0900

    ログイン後の遷移先変更

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update]
   before_action :check_guest, only: :destroy
 
   def check_guest
     if resouce.email == 'guest@example.com'
       redirect_to root_path, alert: 'ゲストユーザーは削除出来ません'
     end
+  end
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image, :remove_image, :image_cache])
+  end
+
+  def after_update_path_for(resource)
+    user_path(current_user.id)
   end
 end
--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -6,4 +6,8 @@ class Users::SessionsController < Devise::SessionsController
     sign_in @user
     redirect_to user_path(@user.id)
   end
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user.id)
+  end
 end
