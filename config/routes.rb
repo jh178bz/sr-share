a0c52cacad41a8f2500080edfca3b8a0768e6544
Jou Hattori <j.h178bz@gmail.com>
2020-08-30 08:59:09 +0900

    follow関連のview作成[修正余地あり]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users,controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    sessions: 'users/sessions'
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
@@ -14,4 +15,5 @@ Rails.application.routes.draw do
       get :following, :followers
     end
   end
+  resources :relationships,       only: [:create, :destroy]
 end
