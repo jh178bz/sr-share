c683a6315640d92d1f87add111e1ee9abedbe50b
Jou Hattori <j.h178bz@gmail.com>
2020-09-02 17:27:46 +0900

    category_idsというカラムはない為validationは与えない

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,6 @@
 class Item < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
   validates :content, presence: true, length: { maximum: 300 }
-  validates :category_ids, presence: true
   has_many :category_items, dependent: :destroy
   has_many :categories, through: :category_items
   accepts_nested_attributes_for :category_items
