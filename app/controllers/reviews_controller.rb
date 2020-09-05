c699b7b01d3694de3f96be923c624ebb2274af4f
Jou Hattori <j.h178bz@gmail.com>
2020-09-05 19:24:12 +0900

    controllerの追加

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,39 @@
+class ReviewsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :find_item
+
+  def show
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.item_id = @item.id
+    if @review.save
+      flash[:notice] = "レビューを投稿しました"
+      redirect_to item_path(@item.id)
+    else
+      flash.now[:notice] = "作成に失敗しました"
+      render 'new'
+    end
+  end
+
+  def destroy
+    Review.find(params[:id]).destroy
+    flash[:notice] = "レビューを削除しました"
+    redirect_to item_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:title, :content, :image, :rate, :item_id, :user_id)
+  end
+
+  def find_item
+    @item = Item.find(params[:item_id])
+  end
+end
