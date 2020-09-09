b01d6b8918410cddde3abb596df7d4a5d8ba329e
Jou Hattori <j.h178bz@gmail.com>
2020-09-09 20:57:59 +0900

    お気に入り機能controller作成

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,22 @@
+class FavoritesController < ApplicationController
+  before_action authenticate_user!
+
+  def create
+    @item = Item.find(params[:item_id])
+    @user = @item.user
+    current_user.favorite(@item)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
+  end
+
+  def destroy
+    @item = Item.find(params[:item_id])
+    current_user.favorites.find_by(item_id: @item.id)
+    respond_to do |format|
+      format.html { redirect_to request.referrer || root_url }
+      format.js
+    end
+  end
+end
