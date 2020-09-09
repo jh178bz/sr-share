3cf6a630da77d8067951563b323af80390305448
Jou Hattori <j.h178bz@gmail.com>
2020-09-09 20:57:04 +0900

    favoriteモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_04_230543) do
+ActiveRecord::Schema.define(version: 2020_09_09_084932) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,26 @@ ActiveRecord::Schema.define(version: 2020_09_04_230543) do
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_category_items_on_category_id"
     t.index ["item_id"], name: "index_category_items_on_item_id"
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "review_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["review_id"], name: "index_comments_on_review_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_favorites_on_item_id"
+    t.index ["user_id", "item_id"], name: "index_favorites_on_user_id_and_item_id", unique: true
+    t.index ["user_id"], name: "index_favorites_on_user_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -88,6 +108,10 @@ ActiveRecord::Schema.define(version: 2020_09_04_230543) do
 
   add_foreign_key "category_items", "categories"
   add_foreign_key "category_items", "items"
+  add_foreign_key "comments", "reviews"
+  add_foreign_key "comments", "users"
+  add_foreign_key "favorites", "items"
+  add_foreign_key "favorites", "users"
   add_foreign_key "reviews", "items"
   add_foreign_key "reviews", "users"
 end
