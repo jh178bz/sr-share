37b1524d21dadcbb7b5b4b65f4e4a005bb549b92
Jou Hattori <j.h178bz@gmail.com>
2020-09-10 19:13:03 +0900

    typo修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,9 +1,8 @@
 class FavoritesController < ApplicationController
-  before_action authenticate_user!
+  before_action :authenticate_user!
 
   def create
     @item = Item.find(params[:item_id])
-    @user = @item.user
     current_user.favorite(@item)
     respond_to do |format|
       format.html { redirect_to request.referrer || root_url }
@@ -13,7 +12,7 @@ class FavoritesController < ApplicationController
 
   def destroy
     @item = Item.find(params[:item_id])
-    current_user.favorites.find_by(item_id: @item.id)
+    current_user.favorites.find_by(item_id: @item.id).destroy
     respond_to do |format|
       format.html { redirect_to request.referrer || root_url }
       format.js
