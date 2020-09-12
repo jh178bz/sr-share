75f7b8f036e1f40bdc54e32e86606a6b01fbebb1
Jou Hattori <j.h178bz@gmail.com>
2020-09-12 10:04:02 +0900

    通知モデルにvalidationの追加

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,4 +2,7 @@ class Notification < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   belongs_to :visiter, class_name: 'User', foreign_key: 'visiter_id', optional: true
   belongs_to :visited, class_name: 'User', foreign_key: 'visited_id', optional: true
+  validates :visiter_id, presence: true
+  validates :visited_id, presence: true
+  validates :action,     presence: true
 end
