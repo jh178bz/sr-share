52152ff8e76bd09fd96626de6736709d221a3d42
Jou Hattori <j.h178bz@gmail.com>
2020-09-19 12:11:29 +0900

    user表示件数を制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,27 +9,27 @@ class UsersController < ApplicationController
   def index
     @search_word = params[:q][:name_cont] if params[:q]
     @q = User.all.ransack(params[:q])
-    @users = @q.result(distinct: true).page(params[:page])
+    @users = @q.result(distinct: true).paginate(page: params[:page], per_page: 12)
   end
 
   def following
     @title = "フォロー"
     @user  = User.find(params[:id])
-    @users = @user.following.paginate(page: params[:page])
+    @users = @user.following.paginate(page: params[:page], per_page: 10)
     render 'show_follow'
   end
 
   def followers
     @title = "フォロワー"
     @user  = User.find(params[:id])
-    @users = @user.followers.paginate(page: params[:page])
+    @users = @user.followers.paginate(page: params[:page], per_page: 10)
     render 'show_follow'
   end
 
   def favorites
     @title = "お気に入り"
     @user = User.find(params[:id])
-    @favorites = @user.favorites.paginate(page: params[:page])
+    @favorites = @user.favorites.paginate(page: params[:page], per_page: 6)
     render 'show_favorite'
   end
 end
