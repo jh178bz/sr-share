65929cf9710aa6c830c5a417034e47f534d3c80c
Jou Hattori <j.h178bz@gmail.com>
2020-09-20 12:40:40 +0900

    rubocop修正

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def home
-    @items = Item.paginate(page:params[:page], per_page: 4)
-    @reviews = Review.includes(:item).paginate(page:params[:page], per_page: 3)
+    @items = Item.paginate(page: params[:page], per_page: 4)
+    @reviews = Review.includes(:item).paginate(page: params[:page], per_page: 3)
   end
 
   def about
