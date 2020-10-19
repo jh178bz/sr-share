a5f0dc76ea8cb355b8b5230830d6b87829e3e605
Jou Hattori <j.h178bz@gmail.com>
2020-10-19 17:52:07 +0900

    validationの追加

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 class Category < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
+  validates :id, presence: true
   has_many :category_items, dependent: :destroy
   has_many :items, through: :category_items
   accepts_nested_attributes_for :category_items
--- app/models/maker.rb
+++ app/models/maker.rb
@@ -1,5 +1,8 @@
 class Maker < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
+  validates :id, presence: true
   has_many :maker_items, dependent: :destroy
   has_many :items,through: :maker_items
   accepts_nested_attributes_for :maker_items
+  mount_uploader :image, ImageUploader
 end
