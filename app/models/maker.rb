58a25ea97b2e6dd7b5255bdb9015f3358224a81d
Jou Hattori <j.h178bz@gmail.com>
2020-10-19 16:57:30 +0900

    makerモデルの中間テーブル作成

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -1,2 +1,5 @@
 class Maker < ApplicationRecord
+  has_many :maker_items, dependent: :destroy
+  has_many :items,through: :maker_items
+  accepts_nested_attributes_for :maker_items
 end
---
+++ spec/factories/maker_items.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :maker_item do
+    
+  end
+end
