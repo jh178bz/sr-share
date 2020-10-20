89a58111a661ab70c70e89b1a63f82266450654b
Jou Hattori <j.h178bz@gmail.com>
2020-10-20 10:50:21 +0900

    maker機能実装

--- app/models/maker.rb
+++ app/models/maker.rb
@@ -2,7 +2,7 @@ class Maker < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }, uniqueness: true
   validates :id, presence: true
   has_many :maker_items, dependent: :destroy
-  has_many :items,through: :maker_items
+  has_many :items, through: :maker_items
   accepts_nested_attributes_for :maker_items
   mount_uploader :image, ImageUploader
 end
--- spec/models/maker_item_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe MakerItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
