c71fdaec4ea0a1ca7723a539408c777c9c5f1f8c
jh178bz <j.h178bz@gmail.com>
2020-10-26 08:33:45 +0000

    awsデプロイ設定

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider              => 'AWS',
-      :region                => ENV['S3_REGION'],
-      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
-      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+      :region                => 'ap-northeast-1',
+      :aws_access_key_id     => Rails.application.credentials.aws[:access_key_id],
+      :aws_secret_access_key => Rails.application.credentials.aws[:secret_access_key]
     }
-    config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_directory     =  'sr-share-jh178bz'
   end
 end
